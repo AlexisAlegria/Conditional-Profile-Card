ac4f333558fd1db966d22fb4a2d6b032adc2027d
AlexisAlegria <alexisalegriav@gmail.com>
2021-09-27 22:48:19 +0000

    adding conditionals in index.js file

--- src/js/index.js
+++ src/js/index.js
@@ -28,6 +28,11 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  if (variables.name == null) variables.name = "Alexis";
+  if (variables.lastname == null) variables.lastname = "Alegria";
+  if (variables.role == null) variables.role = "Student in 4Geeks";
+  if (variables.country == null) variables.country = "Chile";
+  if (variables.city == null) variables.city = "Santiago";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
@@ -64,11 +69,11 @@ window.onload = function() {
     github: "alesanchezr",
     linkedin: null,
     instagram: null,
-    name: "Your name",
-    lastname: "Your lastname",
-    role: "Role",
-    country: "Country",
-    city: "City"
+    name: null,
+    lastname: null,
+    role: null,
+    country: null,
+    city: null
   };
   render(window.variables); //render the card for the first time
 
